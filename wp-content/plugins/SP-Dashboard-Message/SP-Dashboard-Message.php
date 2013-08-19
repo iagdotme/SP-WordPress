5e1e991db4f942edc9560382e2b3407c4ea6bdc8
Ian Anderson Gray <ian@iag.me>
2013-08-19 15:02:36 +0100

    Add SP-Dashboard-Message Plugin

---
+++ wp-content/plugins/SP-Dashboard-Message/SP-Dashboard-Message.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Dashboard Message
+Plugin URI: http://selectperformers.com/
+Description: Add Dashboard Message
+Author: Ian Anderson Gray
+Version: 1.0.0
+Author URI: http://selectperformers.com/
+*/
+function sp_dashboard_widget_function() {
+	// Entering the text between the quotes
+	echo "<h4>Progress</h4><ul>
+	<li><strong>Release Date:</strong> Still in Development</li>
+	<li><strong>Author:</strong> Ian Anderson Gray / Select Performers.</li>
+	<li><strong>Hosting provider:</strong> <a href=\"http://selectperformers.com/\">Select Performers</a></li>
+	</ul><hr />
+	<iframe width='100%' height='300' frameborder='0' src='https://docs.google.com/spreadsheet/pub?key=ADDKEYHERE";
+}
+
+function sp_add_dashboard_widgets() {
+	wp_add_dashboard_widget('wp_dashboard_widget', 'Technical information', 'sp_dashboard_widget_function');
+}
+
+add_action('wp_dashboard_setup', 'sp_add_dashboard_widgets' );
+
+?>
